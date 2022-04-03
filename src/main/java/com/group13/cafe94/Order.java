84d1dd9bde95db023cc6c1ca52295af885176a3d
Temi Afeye <temiafeye@live.com>
2022-04-03 18:11:58 +0100

    replaced persisted order with place order

--- src/main/java/com/group13/cafe94/Order.java
+++ src/main/java/com/group13/cafe94/Order.java
@@ -1,15 +1,15 @@
 package com.group13.cafe94;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order{
     private int orderID;
     private String customerId;
     private String typeOfOrder;
     private String[] orderList;
     private String orderStatus;
-
-    public void placeOrder(){
-
-    }
 
     public String getOrderStatus() {
         return orderStatus;
@@ -19,8 +19,13 @@ public class Order{
         this.orderStatus = orderStatus;
     }
 
-    public String[] getOrderList() {
-        return orderList;
+    public String getOrderList() {
+        //returns a comma separate value of all the items in order list
+        String ordList = "";
+        for(String o : orderList){
+            ordList += o + ",";
+        }
+        return ordList;
     }
 
     public void setOrderList(String[] orderList) {
@@ -52,14 +57,44 @@ public class Order{
     }
 
     public String[] getMenu(){
-        return null;
+        return Menu.getMenuItems();
     }
 
     public String[] getOutstandingOrders(){
+        //pending, cancelled, completed
+        List<String> outstandingOrders = new ArrayList<String>();
+
+        String sql = "SELECT order_id FROM orders WHERE customer_id = "+getCustomerId();
+        sql += " and orderStatus = "+ getOrderStatus();
+        try {
+            Connection con = DBConnectionHandler.getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while ( rs.next() ) {
+                String ord_id = rs.getString("order_id");
+                outstandingOrders.add(ord_id);
+            }
+            con.close();
+            String[] outsListArray = new String[ outstandingOrders.size() ];
+            return outstandingOrders.toArray( outsListArray );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
-    public void persistOrderData(){
+    public void updateOrderStatus(){
+        // update the status of an order
+        String sql = "UPDATE orders SET orderStatus="+getOrderStatus();
+        sql += "WHERE order_id="+getOrderID();
+        System.out.println("Status Updated : " +DataPersistanceHandler.persistToSpecificTable(sql));
+    }
+
+    public void placeOrder(){
+        //insert an order into the database
+        String sql = "INSERT INTO orders(order_id,customer_id,typeOfOrder,orderStatus,order_List)";
+        sql += " VALUES("+getOrderID()+","+getCustomerId()+","+getTypeOfOrder()+","+getOrderStatus()+","+getOrderList()+")";
+        System.out.println("Data Inserted : " +DataPersistanceHandler.persistToSpecificTable(sql));
 
     }
 
